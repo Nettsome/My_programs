a10c0e43fd891a4bde722effb62464bee5eb4ed7
Red Tyn <Tynred@outlook.com>
2023-04-02 13:50:29 +0300

    task1 completed

--- 4_lesson/main_List.cpp
+++ 4_lesson/main_List.cpp
@@ -6,9 +6,7 @@
 
 using namespace std;
 
-
-
-int main()
+void task1()
 {
 	setlocale(LC_ALL, "Rus");
 
@@ -23,8 +21,23 @@ int main()
 	list_elem* group2 = nullptr;
 
 	separation(list, group1, group2);
+	cout << "Group 1:" << endl;
 	while (pop(group1, stud))
 	{
 		cout << stud.name << " " << stud.group << endl;
 	}
+
+
+	cout << "\nGroup 2:" << endl;
+	while (pop(group2, stud))
+	{
+		cout << stud.name << " " << stud.group << endl;
+	}
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+
+	task1();
 }
